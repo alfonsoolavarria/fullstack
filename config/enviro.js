3eda64eff6b43779b95982ab9520fb77196be088
Alfonso Olavarria <alfonsojn15@gmail.com>
2017-11-13 13:58:30 -0400

    Update enviro.js

--- config/enviro.js
+++ config/enviro.js
@@ -1,14 +1,14 @@
 /*ACCESS
-Master Key: 0ae7ebf4-0021-4a48-a8a7-d40502cd35de
-App ID: 8a70d849-410c-409f-9be4-b199125afb10
-Parse-dashboard:  https://timesapp.azurewebsites.net/parse-dashboard
-Url-parse:  https://timesapp.azurewebsites.net/parse*/
+Master Key: ''
+App ID: ''
+Parse-dashboard:  https://url/parse-dashboard
+Url-parse:  https://url/parse*/
 
 
 module.exports = {
-  'PARSE_APP_ID': '8a70d849-410c-409f-9be4-b199125afb10',
-  'PARSE_SERVER_URL' :  'https://timesapp.azurewebsites.net/parse',
-  'PARSE_MASTER_KEY' : '0ae7ebf4-0021-4a48-a8a7-d40502cd35de',
+  'PARSE_APP_ID': 'appid',
+  'PARSE_SERVER_URL' :  'https://url/parse',
+  'PARSE_MASTER_KEY' : 'masterkey',
   'PORT' : 1339,
   'PARSE_MOUNT':'/parse',
   //'DATABASE_URI':'mongodb://localhost:27017/dev2'
