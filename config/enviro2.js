533b90e2d8f0e87e7b885cc4068e2a09692794fc
Alfonso Olavarria <alfonsojn15@gmail.com>
2017-11-13 13:58:57 -0400

    Update enviro2.js

--- config/enviro2.js
+++ config/enviro2.js
@@ -1,7 +1,7 @@
 module.exports = {
-  'PARSE_APP_ID': 'app',
+  'PARSE_APP_ID': 'appid',
   'PARSE_SERVER_URL' :  'http://localhost:1339/parse',
-  'PARSE_MASTER_KEY' : '123dev',
+  'PARSE_MASTER_KEY' : 'masterkey',
   'PORT' : 1339,
   'PARSE_MOUNT':'/parse',
   'DATABASE_URI':'mongodb://localhost:27017/dev2'
